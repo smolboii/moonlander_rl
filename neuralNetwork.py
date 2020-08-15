8f27ad0e88c5d7035feca28d9385379f7ccf9419
loldabigboi <loldabigboi@gmail.com>
2020-08-15 15:43:15 +1200

    Added device field to NeuralNetwork

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device, '\n')
+
 class NeuralNetwork(nn.Module):
 
-    def __init__(self, n_inputs, layer1_size, layer2_size, layer3_size, n_outputs):
+    def __init__(self, n_inputs, layer1_size, layer2_size, layer3_size, n_outputs, device=device):
 
         super(NeuralNetwork, self).__init__()
 
@@ -20,6 +23,8 @@ class NeuralNetwork(nn.Module):
             nn.ReLU()
         )
         self.fc4 = nn.Linear(layer3_size, n_outputs)
+
+        self.device = device
 
     def forward(self, X):
         x = self.fc1(X)
